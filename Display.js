71ee6ab551b2900f5f1b87d96f3caaa5b09b1e0c
Josh Burlile <josh.burlile@gmail.com>
2022-08-21 11:20:13 -0500

    Display update

--- Display.js
+++ Display.js
@@ -16,10 +16,31 @@ class LinkedList {
             //Also we need to tell them when to stop otherwise they will just run off the end of the train
             while(runner !== null){
                 //Since the runner is set to the current node, its value will be equal to the value of the node they                 are currently on
-                 sum+=runner.data
+                sum+=runner.data
                 //Tell our attendant to move to the next car
                 runner=runner.next
             }
             return sum
         }
+    // Display the values of all the nodes in the list
+    display() {
+        var ListString = ""; // String that will contain values in the list (empty at first)
+        // Edge case: list is empty
+        if (this.head == null) {
+            return "Empty String";
+        }
+        // If at least one node:
+        ListString += this.head.value // Concatenating the first value in the list
+        // Adding: ", " + value; to following values after first
+        var runner = this.head.next;
+        while (runner.value != null) {
+            ListString += ", " + runner.value; // add node's value to the list
+            runner = runner.next; // moving the runner to the next node
+        }
+        return ListString;
+    }
 }
+
+var mySLL = new LinkedList();
+mySLL.findSum(5);
+console.log(mySLL.display());
